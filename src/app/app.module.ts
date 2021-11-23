2ee91e6886a927891886abd3249958d2ebec581c
Paulo Mujollo <paulinhomujollo@gmail.com>
2021-11-23 09:57:52 -0300

    part 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+//module routing
+import { AppRoutingModule } from './app-routing.module';
+
+//componentes
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
