459262dd96660c7473b30957e5100bb18c6c2151
Paulo Mujollo <paulinhomujollo@gmail.com>
2021-11-29 11:10:33 -0300

    part 6 finalizada

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,13 +25,20 @@ export class HeroDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHero();
-}
+  }
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
                     .subscribe(hero => this.hero = hero);
-}
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
 
   goBack(): void {
     this.location.back();
